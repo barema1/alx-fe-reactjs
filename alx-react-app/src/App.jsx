3fb5fcfa6b1604b2aa299b9bc6660cc01cd80f87
Barema Gael <baremagael@gmail.com>
2025-11-09 19:35:15 +0200

    Task 3 commit update3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -10,9 +10,9 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile
-        name="Gael"
-        age="21"
-        bio="Loves basketball and gyming."
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography."
       />
       <Footer />
     </>
