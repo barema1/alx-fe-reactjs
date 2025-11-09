9db0f98fdc8577c76ff979147b71355c81983e75
Barema Gael <baremagael@gmail.com>
2025-11-09 19:30:59 +0200

    Task 3 commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,12 +2,18 @@ import './App.css';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <>
       <Header />
       <MainContent />
+      <UserProfile
+        name="Gael"
+        age="21"
+        bio="Loves basketball and gyming."
+      />
       <Footer />
     </>
   );
