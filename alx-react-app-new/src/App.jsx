6920bc7ae37dc93b12ff927aba95736f43270d15
Barema Gael <baremagael@gmail.com>
2025-11-10 14:03:36 +0200

    updated alx-react-app-new3

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         age="25"
         bio="Loves hiking and photography."
       />
+      <Counter />
       <Footer />
     </>
   );
