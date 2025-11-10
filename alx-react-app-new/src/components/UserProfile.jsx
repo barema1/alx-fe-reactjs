825c2e49464cfc34ce14beee6f3727e2473b9059
Barema Gael <baremagael@gmail.com>
2025-11-10 13:52:17 +0200

    updated alx-react-app-new2

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 const UserProfile = (props) => {
   return (
     <div style={{ border: '1px solid gray', padding: '16px', margin: '16px auto', maxWidth: '400px', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
-      <h2 style={{ color: 'blue', marginBottom: '8px' }}>{props.name}</h2>
+      <h2 style={{ color: 'blue', marginBottom: '10px' }}>{props.name}</h2>
       <p style={{ marginBottom: '4px' }}>
         Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span>
       </p>
