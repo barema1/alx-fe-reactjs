9bc29b1894dbd558a8e95017d6f72190ba389a4e
Barema Gael <baremagael@gmail.com>
2025-11-10 15:02:29 +0200

    updated alx-react-app-new7

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
-const UserProfile = (props) => {
+import { useContext } from 'react';
+import UserContext from '../UserContext';
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
     <div style={{ border: '1px solid gray', padding: '16px', margin: '16px auto', maxWidth: '400px', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
-      <h2 style={{ color: 'blue', marginBottom: '10px' }}>{props.name}</h2>
-      <p style={{ marginBottom: '4px' }}>
-        Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span>
-      </p>
-      <p>{props.bio}</p>
+      <h2 style={{ color: 'blue', marginBottom: '8px' }}>{userData.name}</h2>
+      <p style={{ marginBottom: '4px' }}>Email: {userData.email}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
