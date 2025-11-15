662e729c687cc98c7e6542b8a15872facd8dba4f
Barema Gael <baremagael@gmail.com>
2025-11-15 23:19:25 +0200

    added task 1

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,26 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div style={{ maxWidth: "720px", margin: "0 auto" }}>
-      <h1 style={{ textAlign: "center" }}>Recipe Sharing App</h1>
+    <Router>
+      <div style={{ maxWidth: "720px", margin: "0 auto" }}>
+        <h1 style={{ textAlign: "center" }}>Recipe Sharing App</h1>
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
