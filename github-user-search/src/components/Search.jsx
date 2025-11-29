95d6da1d11bc812e8665e35eb3574dd647bc5d2a
Barema Gael <baremagael@gmail.com>
2025-11-29 20:29:25 +0200

    added task 2

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -19,7 +19,7 @@ function Search() {
       const data = await fetchAdvancedUsers(username, location, minRepos);
       setResults(data.items);
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     }
 
     setLoading(false);
