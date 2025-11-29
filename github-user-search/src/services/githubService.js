ca0f5df653ab9112936156a830939e88c4b9facf
Barema Gael <baremagael@gmail.com>
2025-11-29 20:23:53 +0200

    added task 2

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+
+export async function fetchUserData(username) {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+}
+
 export async function fetchAdvancedUsers(username, location, minRepos) {
   let query = "";
 
