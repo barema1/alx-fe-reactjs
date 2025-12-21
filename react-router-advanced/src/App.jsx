ecfeab5f780416d6e0b9ce03ee96aaf228383358
Barema Gael <baremagael@gmail.com>
2025-12-21 22:23:59 +0200

    added task3

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,36 +1,23 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile';
-import ProfileDetails from './components/ProfileDetails';
-import ProfileSettings from './components/ProfileSettings';
-import BlogPost from './components/BlogPost';
 import ProtectedRoute from './components/ProtectedRoute';
+import BlogPost from './components/BlogPost';
 
 function App() {
-  const isAuth = true;
+  const isAuthenticated = true;
 
   return (
     <Router>
-      <nav>
-        <Link humanitarian to="/">Home</Link> | 
-        <Link to="/profile">Profile (Protected)</Link> | 
-        <Link to="/blog/1">Blog Post 1</Link>
-      </nav>
-
       <Routes>
-        <Route path="/" element={<div><h1>Home Page</h1></div>} />
-        
+        <Route path="/" element={<div>Home Page</div>} />
         <Route 
           path="/profile/*" 
           element={
-            <ProtectedRoute isAuthenticated={isAuth}>
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
               <Profile />
             </ProtectedRoute>
-          }
-        >
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
-
+          } 
+        />
         <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
