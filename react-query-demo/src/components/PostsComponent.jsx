b3a0b29237bbd6f7e1995a8c5e80e0a63fb9932c
Barema Gael <baremagael@gmail.com>
2025-12-21 22:08:17 +0200

    added task2

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,39 @@
+import { useQuery } from 'react-query';
+
+const fetchPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+const PostsComponent = () => {
+  const { 
+    data, 
+    error, 
+    isLoading, 
+    isError, 
+    refetch 
+  } = useQuery('posts', fetchPosts, {
+    cacheTime: 1000 * 60 * 5,
+    staleTime: 1000 * 60,
+  });
+
+  if (isLoading) return <div>Loading posts...</div>;
+  if (isError) return <div>Error: {error.message}</div>;
+
+  return (
+    <div>
+      <button onClick={() => refetch()}>Refetch Posts</button>
+      {data.map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostsComponent;
