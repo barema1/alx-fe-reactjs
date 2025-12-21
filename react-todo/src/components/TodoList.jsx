839719c134b91f4009a5f404c53023285416525c
Barema Gael <baremagael@gmail.com>
2025-12-21 22:42:39 +0200

    added task4

---
+++ react-todo/src/components/TodoList.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import AddTodoForm from './AddTodoForm';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Build a Project', completed: false }
+  ]);
+
+  const addTodo = (text) => {
+    const newTodo = { id: Date.now(), text, completed: false };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(todos.map(todo => 
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <AddTodoForm addTodo={addTodo} />
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <span 
+              onClick={() => toggleTodo(todo.id)}
+              style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
